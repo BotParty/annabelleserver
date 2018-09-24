7027476cc3093becd9cf7b2d73be0048144bd4ce
Luke Keyes <lukeyes@hotmail.com>
2018-09-24 00:14:56 -0500

    Removing GUI, headless server just forwards commands

--- src/main/java/org/botparty/annabelle/Application.java
+++ src/main/java/org/botparty/annabelle/Application.java
@@ -1,18 +1,21 @@
 package org.botparty.annabelle;
 
-import javax.swing.*;
-
 public class Application {
 
     public static void main(String[] args) {
-        SwingUtilities.invokeLater(
-                new Runnable() {
-                    @Override
-                    public void run() {
-                        ChatServer.getInstance();
-                        AnnabelleFrame.createAndShowGUI();
-                    }
-                }
-        );
+
+        new Runnable() {
+            @Override
+            public void run() {
+                // start chat instance
+                ChatServer.getInstance();
+
+                // run indefinitely until ctrl+c
+                //noinspection InfiniteLoopStatement
+                do {
+                    Thread.yield();
+                } while (true);
+            }
+        };
     }
 }
